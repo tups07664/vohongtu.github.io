c4399c28ba5807d2bae84bf0f75f72505f6cd554
tups07664 <44158261+tups07664@users.noreply.github.com>
2018-10-19 22:52:38 +0700

    asm

---
+++ js/myjs2.js
@@ -0,0 +1,33 @@
+$('.n1').click(function(){
+		$('body').animate({scrollTop:850});
+		
+		return false;
+	})
+
+$('.n2').click(function(){
+		$('body').animate({scrollTop:1700});
+		
+		return false;
+	})
+
+
+$('.n3').click(function(){
+		$('body').animate({scrollTop:2500});
+		
+		return false;
+	})
+$('.n4').click(function(){
+		$('body').animate({scrollTop:3900});
+		
+		return false;
+	})
+$('.n5').click(function(){
+		$('body').animate({scrollTop:5450});
+		
+		return false;
+	})
+$('.n6').click(function(){
+		$('body').animate({scrollTop:6300});
+		
+		return false;
+	})
